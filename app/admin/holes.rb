75ffe99271e426b416df75a92fee36a980b67fa6
anonymous anonymous <terry.goguen@kellogg.northwestern.edu>
2022-03-11 18:53:35 +0000

    Whitelist columns in Hole for ActiveAdmin

--- app/admin/holes.rb
+++ app/admin/holes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Hole do
 
+ permit_params :yardage, :par, :handicap, :polygon_map, :center_of_green_longitude, :center_of_green_latitude, :course_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
