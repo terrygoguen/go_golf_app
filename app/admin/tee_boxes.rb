6849abea2d61630cd130f0aed0ef0d6d3b1a893e
anonymous anonymous <terry.goguen@kellogg.northwestern.edu>
2022-03-11 18:53:37 +0000

    Whitelist columns in TeeBox for ActiveAdmin

--- app/admin/tee_boxes.rb
+++ app/admin/tee_boxes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register TeeBox do
 
+ permit_params :hole_id, :color, :yardage
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
