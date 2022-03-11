839eae02610ae1524458f8ea8112f9f816a3f10b
anonymous anonymous <terry.goguen@kellogg.northwestern.edu>
2022-03-11 18:53:21 +0000

    Direct association between users and course_likes

--- app/models/course_like.rb
+++ app/models/course_like.rb
@@ -1,6 +1,8 @@
 class CourseLike < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :course_likes,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
